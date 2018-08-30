9e087eebfa7a4a7262e857e920ec8b748c371442
ssrokyz <ssrokyz@gmail.com>
2018-08-30 20:00:58 +0900

    Incomplete add of amp_tf_bunch ftn

--- ss_phonopy.py
+++ ss_phonopy.py
@@ -352,6 +352,99 @@ def calc_amp_tf(phonon, nn, verbose = False):
 
     return phonon
 
+def calc_amp_tf_bunch(phonon, nn, verbose = False):
+    """ Calculate Force Constant with AMP with tensorflow (fast version) """
+    ################### all same from now 
+    import subprocess as sp
+    import numpy as np
+    import pickle
+    np.set_printoptions(threshold=np.nan)
+        
+    delta = np.linalg.norm(phonon.get_displacements()[0][1:4])
+    directions = phonon.get_displacement_directions()
+    N = phonon.get_supercell_matrix()[0][0]
+    image_num = len(directions)
+    tf = {0:'nonsym', 1:'sym'}
+    sym = tf[phonon._is_symmetry]
+    job_name = "x" + str(N) + "_d" + str(delta) + "_" + sym
+    import sys
+    if (sys.version_info > (3,0)):
+        pwd = str((sp.check_output("pwd"))[:-1])[2:-1]
+    else:
+        pwd = str((sp.check_output("pwd"))[:-1])
+    calc_dir = pwd + "/calcs/" + job_name
+    sp.call(["rm -rf " + calc_dir + "/poscars"], shell = True)
+    sp.call(["mkdir -p " + calc_dir + "/poscars"], shell = True)
+    sp.call(["cp POSCAR-* SPOSCAR " + calc_dir + "/poscars"], shell = True)
+    sp.call(["rm -rf POSCAR-* SPOSCAR"], shell = True)
+
+    try:
+        phonon = pickle.load(open("pickle-"+job_name+".p", "rb"))
+        if phonon.get_force_constants() is None:
+            raise ValueError
+        ################### all same until now 
+    except:
+        print("******* There is no saved pickle file. ********".center(80))
+        print("******* AMP calc will be carried. ********".center(80))
+
+        forces = []
+        for i in range(image_num):
+            ndir = "pos" + str(i+1).zfill(4) + "_atom" + str(directions[i][0]).zfill(4) + "_direc" + \
+                   str(directions[i][1]) + str(directions[i][2]) + str(directions[i][3])
+            sp.call(["rm", "-rf", calc_dir+"/BU-"+ndir])
+            sp.call(["mv", calc_dir+"/"+ndir, calc_dir+"/BU-"+ndir])
+            sp.call(["mkdir", "-p", calc_dir+"/"+ndir])
+            sp.call(["cp", calc_dir+"/poscars/POSCAR-"+str(i+1).zfill(3), calc_dir+"/"+ndir])
+
+            ########### calculate forces & atomic energies with amp ############
+            from ase.io import read
+            atoms = read(calc_dir+"/"+ndir+"/POSCAR-"+str(i+1).zfill(3),
+                         format = "vasp")
+            atoms.set_pbc(True)
+            calc = nn
+            atoms.set_calculator(calc)
+            #print(atoms.__dict__)
+
+            #********** numerical force must precede ***********
+            force_now = [(np.squeeze(calc.calculate_numerical_forces(atoms), axis=2)).tolist()] # alternative
+            #force_now = [atoms.get_forces(apply_constraint=False).tolist()] # alternative
+            #stress_now = calc.calculate_numerical_stress(atoms) # just in case
+            #********** energy calculation ***********
+            energy_now = atoms.get_potential_energy()
+            energies_now = atoms.get_potential_energies()
+            #********** force information restore ***********
+            atoms._calc.results['forces'] = np.asarray(force_now[0])
+            if verbose:
+                print(energies_now)
+                print(atoms._calc.results['forces'])
+            forces.extend(force_now)
+            #********** traj file gen ***********
+            from ase.io.trajectory import Trajectory
+            traj = Trajectory(calc_dir+"/"+ndir+"/output.traj", "w")
+            traj.write(atoms)
+            traj.close()
+            
+            if verbose:
+                print("force_now"+str(i+1)+"\n"+str(np.asarray(force_now)))
+        forces_arr = np.asarray(forces)
+        if verbose:
+            print("\n\n"+"forces"+"\n"+str(forces_arr))
+        phonon.set_forces(forces_arr)
+        phonon.produce_force_constants()
+ 
+        if verbose:
+            print("\n\ndisplacement_dataset =>\n\n")
+            print(phonon.get_displacement_dataset())
+            print("\n\nforce_constants =>\n\n")
+            print(str(2*N**3)+" x "+str(2*N**3)+" x "+str(3)+" x "+str(3)+" matrix\n\n")
+            print(phonon.get_force_constants())
+ 
+        pickle.dump(phonon, open("pickle-"+job_name+".p", "wb"))
+    else:
+        print("******* Pickle file has been loaded. ********".center(80))
+
+    return phonon
+
 def make_band(path, N_q):
     import numpy as np
     bands = []
