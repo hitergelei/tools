f61a36f3028e8a0c93396ab946b0fe8fe21b75f8
ssrokyz <ssrokyz@gmail.com>
2019-08-13 09:49:32 +0900

    trivial

--- ase-md.py
+++ ase-md.py
@@ -6,41 +6,60 @@ from ase.io.trajectory import Trajectory as Traj
 from ase.md.velocitydistribution import MaxwellBoltzmannDistribution as Max
 from ase.md.velocitydistribution import Stationary
 from ase import units
-from ase.calculators.vasp import Vasp
 from ase.build import make_supercell
 import datetime
 from ss_util import random_position_generator as RPG
 from ase.calculators.lammpsrun import LAMMPS
 import os
 
-## Global params
-label    = "GeTe-liquid"
-atoms    = read('init.vasp')
-temp     = 1100 *units.kB
-timestep = 10 *units.fs
-ttime    = 1000 *units.fs
-ptime    = 1000 *units.fs
-
+#### Global params
+## cell
+label = "lj-Ar-200K-10fs-NPT"
+atoms = read('rlx-lj-Ar-2.51K.traj', -1)
+for i in range(3):
+    for j in range(3):
+        if i != j:
+            atoms.cell[i,j] = 0.
+##
+temp   = 5 *units.kB
+d_t    = 10 *units.fs
+t_step = 5000
 ############# calculator ############
-calc = Vasp()
+# from ase.calculators.vasp import Vasp
+# calc = Vasp()
+# atoms.set_calculator(calc)
+# calc.read_incar()
+# calc.read_kpoints()
+from ase.calculators.lj import LennardJones as LJ
+calc = LJ(epsilon=120 *units.kB, sigma=0.34 *units.nm)
 atoms.set_calculator(calc)
-calc.read_incar()
-from kpoints_gen import get_grid_num, write_KPOINTS
-write_KPOINTS(get_grid_num(atoms.get_cell(), 45))
-calc.read_kpoints()
+
+########### Relaxation #############
+# from ase.constraints import StrainFilter as SF
+# sf = SF(atoms)
+# from ase.optimize.bfgslinesearch import BFGSLineSearch as BFGSLS
+# opti_posi = BFGSLS(
+    # sf,
+    # logfile='rlx-log'+label+'.txt',
+    # trajectory = 'rlx-'+label+'.traj',
+    # )
+# opti_posi.run(1e-3)
     
 ########### dynamics ###############
-Max(atoms, temp* 1.0)
+Max(atoms, temp * 2)
 Stationary(atoms)
-
 # from ase.md.verlet import VelocityVerlet
-# dyn = VelocityVerlet(atoms, 10*units.fs , trajectory = 'atoms_training_set_'+str(i)+'.traj')  #'dyn' is defined as an object of VV class 
-                                                                #'superrhombo_training_set.traj' will be writen
+# dyn = VelocityVerlet(
+    # atoms,
+    # d_t,
+    # trajectory = label+'.traj',
+    # logfile = 'log_'+label+'.txt',
+    # )
 # from ase.md import Langevin
 # dyn = Langevin(
     # atoms       = atoms,
     # timestep    = 10 *units.fs,
-    # temperature = 1100 *units.kB,
+    # temperature = 100 *units.kB,
     # friction    = 1e-02,
     # trajectory  = label+'.traj',
     # logfile     = 'log_'+label+'.txt',
@@ -48,22 +67,15 @@ Stationary(atoms)
 from ase.md.npt import NPT
 dyn = NPT(
     atoms = atoms,
-    timestep = timestep,
+    timestep = d_t,
     temperature = temp,
     externalstress = 0.,
-    ttime = ttime,
-    pfactor = (ptime)**2 * 100. *units.GPa,
+    ttime = 75 * units.fs,
+    pfactor = (500. *units.fs)**2 * 100. *units.GPa,
     trajectory  = label+'.traj',
     logfile     = 'log_'+label+'.txt',
     )
-## relax option
-dyn.set_fraction_traceless(0.) # 0 --> no shape change but yes volume change
-    
-############ run ###############
-#for i in range(2):        #repeat loop 10 times
-#    pot = superrhombo.get_potential_energy()/numofa    
-#    kin = superrhombo.get_kinetic_energy()/numofa
-#    print('Energy per atom: pot. E. = %f (eV) kin. E. = %f (eV) (Tot. E. = %f (eV))'% (pot, kin, pot + kin))
-dyn.run(steps=200000)     #MD simulation of object 'dyn' is performed by 'run' method of VV class
-# traj.write(atoms)
+# # relax option
+dyn.set_fraction_traceless(0) # 0 --> no shape change but yes volume change
+dyn.run(steps=t_step)     #MD simulation of object 'dyn' is performed by 'run' method of VV class
 
