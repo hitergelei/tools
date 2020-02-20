9913f76bb5725ee63404f763dd98905ef2ca8c64
Youngjae Choi <ssrokyz@gmail.com>
2020-02-20 10:53:39 +0900

    t

--- iterative_work.sh
+++ iterative_work.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-for i in {02..20}
+for i in {00..29}
 do
     rm -rf job_$i
     mkdir job_$i
-    cp 1-training-set-making.py 1-training-set-making.sh INCAR POTCAR cubic-backbone-64.vasp job_$i
-    sed "s/ran_gst_liquid/job_$i/" "1-training-set-making.sh" > job_$i/1-training-set-making.sh
-    qsub job_$i/1-training-set-making.sh
+    ase convert -n $i::30 cubic-not-vacuum.traj job_$i/inits.traj
+    cp run.sh INCAR POTCAR sub_routine.py job_$i/
+    sed "s/ran_gst_liquid/job_$i/" "run.sh" > job_$i/run.sh
+    cd job_$i
+    qsub run.sh
+    cd ..
 done
