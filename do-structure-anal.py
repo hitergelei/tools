19869888d275e797b1003cd92375bb9f15cdc650
ssrokyz <ssrokyz@gmail.com>
2020-03-31 14:40:29 +0900

    t

--- do-structure-anal.py
+++ do-structure-anal.py
@@ -3,11 +3,11 @@
 import numpy as np
 
 # Global parameters
-angle_cutoff = 135.
+angle_cutoff = 145.
 bond_cutoff = 3.7
 bonding_rules = [['Ge','Sb'], ['Te']]
-step_intvl = 400
-alist_ind_list = list(range(0,180001,step_intvl))
+step_intvl = 1
+alist_ind_list = list(range(0,1000,step_intvl))
 # alist_ind_list = list(range(0,2000,step_intvl))
 # alist_ind_list = list(range(10))
 # alist_ind_list = [180000]
@@ -15,7 +15,8 @@ time_intvl = 10 * step_intvl / 1000 # (ps)
 
 from structure_analyses import Structure_analyses
 sa = Structure_analyses(
-    'lmp-result.traj',
+    # 'lmp-result.traj',
+    'GST-melting.traj'
     # 'gst-cubic-md-last50.traj',
     # 'test.traj',
     )
@@ -40,21 +41,21 @@ sa = Structure_analyses(
     # # save_bool=False,
     # )
 
-# sa.plot_chain_length_histo(
-    # angle_cutoff,
-    # bond_cutoff,
-    # bonding_rules,
-    # # alist_ind_list,
-    # [15000]
-    # # load_bool=False,
-    # # save_bool=False,
-    # )
-
-sa.view_chains(
+sa.plot_chain_length_histo(
     angle_cutoff,
     bond_cutoff,
     bonding_rules,
-    [15000],
-    # alist_ind_list,
-    chain_length=2,
+    alist_ind_list,
+    # [15000]
+    # load_bool=False,
+    # save_bool=False,
     )
+
+# sa.view_chains(
+    # angle_cutoff,
+    # bond_cutoff,
+    # bonding_rules,
+    # # [15000],
+    # alist_ind_list,
+    # chain_length=2,
+    # )
