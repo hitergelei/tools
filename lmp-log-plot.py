ce4f6ceeca6c902eaa1811ff6d11684b23c81b53
ssrokyz <ssrokyz@gmail.com>
2020-09-21 23:24:38 +0900

    t

--- lmp-log-plot.py
+++ lmp-log-plot.py
@@ -30,6 +30,8 @@ if __name__ == '__main__':
     log_file = args.log_file
 
     # > Main
+    from lmp2traj import read_lmp_log
+    info = read_lmp_log(log_file)
     import numpy as np
     with open(log_file) as f:
 
@@ -39,81 +41,29 @@ if __name__ == '__main__':
             if words == []:
                 pass
             elif words[0] == 'timestep':
+                if words[1][0] == '$':
+                    words = f.readline().split()
                 dt = float(words[1])
                 break
             elif words[0] == 'Time':
                 dt = float(words[3])
                 break
 
-        # Check the kinds of info we have.
-        info_name = []
-        while 1:
-            line = f.readline()
-            if not line:
-                raise RuntimeError('No an information exists.')
-            words = line.split()
-            if words[1] == 'Step':
-                while 1:
-                    words = f.readline().split()
-                    if words[1] == 'Step':
-                        break
-                    for i in words[::3]:
-                        info_name.append(i)
-                break
-
-        # Start again
-        f.seek(0)
-        while 1:
-            words = f.readline().split()
-            if len(words) < 2:
-                pass
-            elif words[1] == 'Step':
-                t = [int(words[2])]
-                break
-
-        # Gather info
-        info = []
-        new_info = []
-        while 1:
-            line = f.readline()
-            if not line:
-                if new_info:
-                    info.append(new_info)
-                break
-            words = line.split()
-            if len(words) < 2:
-                break
-            elif words[1] == 'Step':
-                t.append(int(words[2]))
-                info.append(new_info)
-                new_info = []
-            else:
-                new_info.extend(words[2::3])
-
-    # Check if the info is complete.
-    if len(info[-1]) != len(info_name):
-        del(info[-1])
-        del(t[-1])
-    if len(info) != len(t):
-        del(t[-1])
-    if len(info) != len(t):
-        raise ValueError("What's wrong? T-T")
-
     # > Post process
-    t = np.array(t) * dt
-    info = np.array(info, dtype=np.float64).T
+    t = np.arange(len(info[list(info.keys())[0]]), dtype=float) * dt
 
     # Remove dummy info.
-    mask_bool = np.std(info, axis=1) != 0.
-    info = info[mask_bool]
-    info_name = np.array(info_name)[mask_bool]
+    avail_info = []
+    for (key, value) in info.items():
+        if np.std(np.array(value, dtype=float)) > 1e-10:
+            avail_info.append(key)
 
     # > Plot
     from matplotlib import pyplot as plt
-    for i in range(len(info_name)):
+    for i in range(len(avail_info)):
         plt.figure()
-        plt.plot(t, info[i], c='k')
-        plt.title(info_name[i], fontsize='x-large')
+        plt.plot(t, info[avail_info[i]], c='k')
+        plt.title(avail_info[i], fontsize='x-large')
         plt.tick_params(axis="both",direction="in", labelsize='x-large')
         plt.grid(alpha=0.2)
     plt.show()
