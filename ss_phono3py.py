70557e5303a67a049c975a6fabc0fbdb04b5d4e9
ssrokyz <ssrokyz@gmail.com>
2021-01-05 10:46:14 +0900

    t

--- ss_phono3py.py
+++ ss_phono3py.py
@@ -5,20 +5,30 @@ from ase.io import read
 import numpy as np
 import pickle as pckl
 
-def calc_vasp(
+def _read_atoms(
     wdir,
     calc,
     ):
+    if calc == 'vasp':
+        atoms = read('{}/vasprun.xml'.format(wdir))
+    elif calc == 'lmp':
+        atoms = read('{}/out.dump'.format(wdir))
+    else:
+        raise ValueError('Unknown calculator ({}) has been provided.'.format(calculator))
+    return atoms
+
+def calc_vasp(
+    wdir,
+    ):
     call('mpiexec.hydra -machinefile $TMPDIR/machines -np $NSLOTS vasp_std > out', shell=True, cwd=wdir)
-    return read('{}/vasprun.xml'.format(wdir))
+    return _read_atoms(wdir, 'vasp')
 
 def calc_lmp(
     wdir,
-    calc,
     ):
     call('lmp-pos2lmp.awk POSCAR > structure.in', shell=True, cwd=wdir)
     call('lmp_mpi -in input-phonon.in > out', shell=True, cwd=wdir)
-    return read('{}/out.dump'.format(wdir))
+    return _read_atoms(wdir, 'lmp')
 
 def _calc_forces(
     wdir,
@@ -40,7 +50,7 @@ def _calc_forces(
         call('cp {} {}'.format(cp_str, wdir), shell=True)
 
     #
-    atoms = calculator(wdir, calc)
+    atoms = calculator(wdir)
     return atoms.get_forces()
 
 def calc_forces(
@@ -75,7 +85,8 @@ def calc_forces(
             phono3py = pckl.load(open('saved-pckls/{}-forces.pckl'.format(job_name), 'rb'))
         except:
             load=False
-            load_calc=True
+            load_fc2_calc=True
+            load_fc3_calc=True
             print('\n\n\n*** NOTE) Failed to load saved-pckls/{}-forces.pckl file. ***\n\n\n'.format(job_name))
         else:
             print('\n\n\n=== NOTE) Loaded: saved-pckls/{}-forces.pckl ===\n\n\n'.format(job_name))
@@ -94,31 +105,39 @@ def calc_forces(
         sc3 = phono3py.get_supercells_with_displacements()
 
         #
-        if load_calc:
+        if load_fc2_calc:
             try:
                 sc2_forces = []
                 for i in range(len(sc2)):
                     wdir = '{}/disp-{:05d}'.format(fc2_path, i+1)
-                    sc2_forces.append(read('{}/vasprun.xml'.format(wdir)).get_forces())
-                sc3_forces = []
-                for i in range(len(sc3)):
-                    wdir = '{}/disp-{:05d}'.format(fc3_path, i+1)
-                    sc3_forces.append(read('{}/vasprun.xml'.format(wdir)).get_forces())
+                    sc2_forces.append(_read_atoms(wdir, calc).get_forces())
             except:
-                print('*** NOTE) Failed to load previous calc results. ***\n\n\n')
-                load_calc=False
+                print('*** NOTE) Failed to load previous fc2 calc results. ***\n')
+                load_fc2_calc=False
                 #
                 call('rm -rf {}'.format(fc2_bu_path), shell=True)
                 call('mv {} {}'.format(fc2_path, fc2_bu_path), shell=True)
                 call('mkdir -p {}'.format(fc2_path), shell=True)
+            else:
+                print('*** NOTE) Loaded the previous fc2 calc results. Be aware! ***\n')
+        if load_fc3_calc:
+            try:
+                sc3_forces = []
+                for i in range(len(sc3)):
+                    wdir = '{}/disp-{:05d}'.format(fc3_path, i+1)
+                    sc3_forces.append(_read_atoms(wdir, calc).get_forces())
+            except:
+                print('*** NOTE) Failed to load previous fc3 calc results. ***\n')
+                load_fc3_calc=False
+                #
                 call('rm -rf {}'.format(fc3_bu_path), shell=True)
                 call('mv {} {}'.format(fc3_path, fc3_bu_path), shell=True)
                 call('mkdir -p {}'.format(fc3_path), shell=True)
             else:
-                print('*** NOTE) Loaded the previous calc results. Be aware! ***\n\n\n')
+                print('*** NOTE) Loaded the previous fc3 calc results. Be aware! ***\n')
 
-        if not load_calc:
-            print('=== NOTE) Start calculations! ===\n\n\n'.format(job_name))
+        if not load_fc2_calc:
+            print('=== NOTE) Start fc2 calculations! ===\n')
             from phonopy.interface.vasp import write_vasp
             sc2_forces = []
             for i in range(len(sc2)):
@@ -126,6 +145,9 @@ def calc_forces(
                 call('mkdir -p {}'.format(wdir), shell=True)
                 write_vasp('{}/POSCAR'.format(wdir), sc2[i])
                 sc2_forces.append(_calc_forces(wdir, calc, cp_files))
+        if not load_fc3_calc:
+            print('=== NOTE) Start fc3 calculations! ===\n')
+            from phonopy.interface.vasp import write_vasp
             sc3_forces = []
             for i in range(len(sc3)):
                 wdir = '{}/disp-{:05d}'.format(fc3_path, i+1)
