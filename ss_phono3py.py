d73ad2bc6e986fab7d2388a34af970b185b68b34
ssrokyz <ssrokyz@gmail.com>
2021-01-05 10:07:57 +0900

    t

--- ss_phono3py.py
+++ ss_phono3py.py
@@ -57,7 +57,17 @@ def calc_forces(
         calc,
         unitcell_f,
         *np.diag(phono3py.phonon_supercell_matrix),
+        *np.diag(phono3py.supercell_matrix),
+        )
+    fc2_name = '{}_{}_sc2-{}-{}-{}'.format(
+        unitcell_f,
+        calc,
         *np.diag(phono3py.phonon_supercell_matrix),
+        )
+    fc3_name = '{}_{}_sc3-{}-{}-{}'.format(
+        unitcell_f,
+        calc,
+        *np.diag(phono3py.supercell_matrix),
         )
 
     if load:
@@ -65,22 +75,18 @@ def calc_forces(
             phono3py = pckl.load(open('saved-pckls/{}-forces.pckl'.format(job_name), 'rb'))
         except:
             load=False
+            load_calc=True
             print('\n\n\n*** NOTE) Failed to load saved-pckls/{}-forces.pckl file. ***\n\n\n'.format(job_name))
         else:
             print('\n\n\n=== NOTE) Loaded: saved-pckls/{}-forces.pckl ===\n\n\n'.format(job_name))
             return phono3py
 
     if not load:
-        print('=== Starting force calculations. ==='.format(job_name))
-        phono3py._job_name = job_name
         calc_dir = 'calcs'
-        path = '{}/{}'.format(calc_dir, job_name)
-        bu_path = '{}/bu-{}'.format(calc_dir, job_name)
-
-        #
-        call('rm -rf {}'.format(bu_path), shell=True)
-        call('mv {} {}'.format(path, bu_path), shell=True)
-        call('mkdir -p {}'.format(path), shell=True)
+        fc2_path = '{}/{}'.format(calc_dir, fc2_name)
+        fc2_bu_path = '{}/bu-{}'.format(calc_dir, fc2_name)
+        fc3_path = '{}/{}'.format(calc_dir, fc3_name)
+        fc3_bu_path = '{}/bu-{}'.format(calc_dir, fc3_name)
 
         #
         phono3py.generate_displacements()
@@ -88,23 +94,50 @@ def calc_forces(
         sc3 = phono3py.get_supercells_with_displacements()
 
         #
-        sc2_forces = []
-        from phonopy.interface.vasp import write_vasp
-        for i in range(len(sc2)):
-            wdir = '{}/fc2/disp-{:05d}'.format(path, i+1)
-            call('mkdir -p {}'.format(wdir), shell=True)
-            write_vasp('{}/POSCAR'.format(wdir), sc2[i])
-            sc2_forces.append(_calc_forces(wdir, calc, cp_files))
-        phono3py.phonon_forces = np.array(sc2_forces)
+        if load_calc:
+            try:
+                sc2_forces = []
+                for i in range(len(sc2)):
+                    wdir = '{}/disp-{:05d}'.format(fc2_path, i+1)
+                    sc2_forces.append(read('{}/vasprun.xml'.format(wdir)).get_forces())
+                sc3_forces = []
+                for i in range(len(sc3)):
+                    wdir = '{}/disp-{:05d}'.format(fc3_path, i+1)
+                    sc3_forces.append(read('{}/vasprun.xml'.format(wdir)).get_forces())
+            except:
+                print('*** NOTE) Failed to load previous calc results. ***\n\n\n')
+                load_calc=False
+                #
+                call('rm -rf {}'.format(fc2_bu_path), shell=True)
+                call('mv {} {}'.format(fc2_path, fc2_bu_path), shell=True)
+                call('mkdir -p {}'.format(fc2_path), shell=True)
+                call('rm -rf {}'.format(fc3_bu_path), shell=True)
+                call('mv {} {}'.format(fc3_path, fc3_bu_path), shell=True)
+                call('mkdir -p {}'.format(fc3_path), shell=True)
+            else:
+                print('*** NOTE) Loaded the previous calc results. Be aware! ***\n\n\n')
 
-        sc3_forces = []
-        for i in range(len(sc3)):
-            wdir = '{}/fc3/disp-{:05d}'.format(path, i+1)
-            call('mkdir -p {}'.format(wdir), shell=True)
-            write_vasp('{}/POSCAR'.format(wdir), sc3[i])
-            sc3_forces.append(_calc_forces(wdir, calc, cp_files))
+        if not load_calc:
+            print('=== NOTE) Start calculations! ===\n\n\n'.format(job_name))
+            from phonopy.interface.vasp import write_vasp
+            sc2_forces = []
+            for i in range(len(sc2)):
+                wdir = '{}/disp-{:05d}'.format(fc2_path, i+1)
+                call('mkdir -p {}'.format(wdir), shell=True)
+                write_vasp('{}/POSCAR'.format(wdir), sc2[i])
+                sc2_forces.append(_calc_forces(wdir, calc, cp_files))
+            sc3_forces = []
+            for i in range(len(sc3)):
+                wdir = '{}/disp-{:05d}'.format(fc3_path, i+1)
+                call('mkdir -p {}'.format(wdir), shell=True)
+                write_vasp('{}/POSCAR'.format(wdir), sc3[i])
+                sc3_forces.append(_calc_forces(wdir, calc, cp_files))
+
+        #
+        phono3py.phonon_forces = np.array(sc2_forces)
         phono3py.forces = np.array(sc3_forces)
 
+        #
         if save:
             call('mkdir saved-pckls', shell=True)
             pckl.dump(
