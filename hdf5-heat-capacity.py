fa33562fa80e356a6db3566186d4101bf71fbff0
ssrokyz <ssrokyz@gmail.com>
2021-01-15 14:40:59 +0900

    t

--- hdf5-heat-capacity.py
+++ hdf5-heat-capacity.py
@@ -6,10 +6,11 @@ def argparse():
     from ss_util import parse_slice
     parser = argparse.ArgumentParser(description = """
     Young-Jae Choi, POSTECH, Korea, Rep. of.
-    Read hdf5 file. Use like ==> "python -i read-hdf5.py 'some hdf5 file'"
+    Get heat capacity from kappa-***.hdf5 file.
     """)
     # Positional arguments
     parser.add_argument('hdf5_file', type=str, help='HDF5 file.')
+    parser.add_argument('prim_file', type=str, help='ASE readable structure of primitive unit-cell.')
 
     return parser.parse_args()
 
@@ -23,12 +24,31 @@ if __name__ == '__main__':
     print(('Code runtime : '+time).center(120))
     print('')
     print('=================================================================================================='.center(120))
-    print('Read hdf5 file. Use like ==> "python -i read-hdf5.py some_hdf5_file"'.center(120))
+    print('Get heat capacity from kappa-***.hdf5 file.'.center(120))
     print('=================================================================================================='.center(120))
     print('')
     ## Argparse
     args = argparse()
     hdf5_file = args.hdf5_file
+    prim_file = args.prim_file
 
     import h5py
     f = h5py.File(hdf5_file, 'r')
+    from ase.io import read
+    V = read(prim_file).get_volume()
+    
+    c_q = np.array(f['heat_capacity'])
+    w_q = np.array(f['weight'])
+    T = np.array(f['temperature'])
+
+    sum_w = np.sum(w_q)
+    c = []
+    print('NOTE) File: {} '.format(hdf5_file))
+    for i in range(len(T)):
+        c.append(np.sum(c_q[i] * np.expand_dims(w_q, axis=1)) /sum_w /V)
+        print('==> C({}K) = {} (eV / K Angs^3) = {} (J / K m^3)'.format(
+            T[i],
+            c[i],
+            c[i] *1.60217662e-19 *1e30,
+            ))
+
