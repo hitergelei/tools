9d777cc1fbf7cdd5760cf851ebb74b32a0a81d24
ssrokyz <ssrokyz@gmail.com>
2021-01-25 17:17:41 +0900

    t

--- ss_phonopy.py
+++ ss_phonopy.py
@@ -190,7 +190,7 @@ def calc_phonon(calculator, phonon, acoustic_sum_rule=True, F_0_correction=False
     """
     # Check if structure is lower triangular cell
     for c in ((0,1), (0,2), (1,2)):
-        if phonon._primitive.get_cell()[c[0],c[1]] != 0.:
+        if phonon._primitive.get_cell()[c[0],c[1]] != 0. and calculator == 'lmp':
             raise ValueError('Please provide lower triangular cell.')
 
     import sys
