5f58da9d26683d941af7aa069339a41717f3aaae
ssrokyz <ssrokyz@gmail.com>
2021-04-16 21:32:25 +0900

    new

---
+++ ssh_all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+#
+from sys import argv
+if len(argv) != 2:
+    raise ValueError('Wrong number of variables')
+
+#
+from os import environ
+from subprocess import call
+if environ['HOSTNAME'] == 'pcs_gpu1':
+    for i in range(2,12):
+        call('ssh pcs_gpu{} "{}"'.format(i, argv[1]), shell=True)
