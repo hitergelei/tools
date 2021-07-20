18bfc215dd3fa72699540e71e0ce93b16a64ef8f
ssrokyz <ssrokyz@gmail.com>
2021-07-20 20:38:05 +0900

    t

--- ss_util.py
+++ ss_util.py
@@ -6,6 +6,17 @@ import random
 from ase.build import make_supercell
 import numpy as np
 
+class bcolors:
+    header = '\033[95m'
+    okblue = '\033[94m'
+    okcyan = '\033[96m'
+    okgreen = '\033[92m'
+    warning = '\033[93m'
+    fail = '\033[91m'
+    endc = '\033[0m'
+    bold = '\033[1m'
+    underline = '\033[4m'
+
 def axisEqual3D(ax):
     extents = np.array([getattr(ax, 'get_{}lim'.format(dim))() for dim in 'xyz'])
     sz = extents[:,1] - extents[:,0]
