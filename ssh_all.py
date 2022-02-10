312a3c2fe51bcb92ff63b122f8bdf53756756e7d
ssrokyz <ssrokyz@gmail.com>
2022-02-10 17:06:29 +0900

    t

--- ssh_all.py
+++ ssh_all.py
@@ -8,7 +8,5 @@ if len(argv) != 2:
 #
 from os import environ
 from subprocess import call
-if environ['HOSTNAME'] == 'pcs_gpu1':
-    for i in range(2,12):
-        if i != 8:
-            call('ssh pcs_gpu{} "{}"'.format(i, argv[1]), shell=True)
+for i in range(2,12):
+    call('ssh pcs_gpu{} "{}"'.format(i, argv[1]), shell=True)
