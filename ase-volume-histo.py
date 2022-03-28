83f4e4ca2204e078c6d2369ed6d6fddbb63769e7
ssrokyz <ssrokyz@gmail.com>
2022-03-28 15:28:30 +0900

    t

--- ase-volume-histo.py
+++ ase-volume-histo.py
@@ -1,47 +1,74 @@
 #!/usr/bin/env python
-
 import numpy as np
-import sys
 
-print("\n")
-print("#######################################################################################".center(120))
-print("")
-print("useage ==> ./ase-volume-histo.py 'atoms list file'".center(120))
-print("           EXAMPLE) ./ase-volume-histo.py vasprun.xml".center(120))
-print("")
-print("#######################################################################################".center(120))
-print("")
-if len(sys.argv) is 2:
-    print("The Number of arguments is correct.".center(120))
-    print("\n")
-else:
-    print(">>>>> ERROR <<<<<     The number of arguments is not correct     >>>>> ERROR <<<<<".center(120))
-    print("\n")
-    sys.exit(1)
+def argparse():
+    import argparse
+    parser = argparse.ArgumentParser(description = """
+    This code will give you the volume and density histogram.
+    """)
+    # Positional arguments
+    parser.add_argument('alist_file', type=str, help='ASE readable atoms list file name.')
+    # Optional arguments
+    parser.add_argument('-n', '--ase_slice', type=str, default=':', help='ASE readable format of slice for atoms list file. [default: ":"]')
+    return parser.parse_args()
 
-from ase.io import read
-alist = read(sys.argv[1], ':')
+if __name__ == '__main__':
+    ## Intro
+    import datetime
+    now = datetime.datetime.now()
+    time = now.strftime('%Y-%m-%d %H:%M:%S')
+    print('')
+    print('>>>>> Code by Young Jae Choi @ POSTECH <<<<<'.center(120))
+    print(('Code runtime : '+time).center(120))
+    print('')
+    print('=================================================================================================='.center(120))
+    print('This code will give you the volume and density histogram.'.center(120))
+    print('=================================================================================================='.center(120))
+    print('')
+    args = argparse()
 
-vol_list = []
-for atoms in alist:
-    vol_list.append(np.linalg.det(atoms.get_cell()))
-# Post process
-anum = len(alist[0])
-vol_list = np.array(vol_list)/anum
-density_arr = 1./vol_list
-average = np.mean(density_arr)
-std = np.std(density_arr)
-    
-## plot
-from matplotlib import pyplot as plt
-font = {'family':'Arial'}
-plt.rc('font', **font)
-n, bins, patches = plt.hist(density_arr, bins=100, facecolor='gray', alpha=0.70)
-max_height = np.sort(n)[-10]
-plt.title('Density Histogram (atoms/Ang^3)', fontsize='x-large')
-plt.xlabel('%d images, average = %.4e, sigma = %.4e' % (len(density_arr), average, std), fontsize='x-large')
-plt.ylabel('population', fontsize='x-large')
-plt.barh(max_height/5, std, height=max_height/50, left=average, color='black')
-plt.tick_params(axis="both",direction="in", labelsize='x-large')
-plt.grid(True)
-plt.show()
+    from ase.io import read
+    alist = read(args.alist_file, args.ase_slice)
+
+    vol_arr = []
+    n_arr = []
+    for atoms in alist:
+        vol_arr.append(np.linalg.det(atoms.get_cell()))
+        n_arr.append(len(atoms))
+    vol_arr = np.array(vol_arr)
+    n_arr = np.array(n_arr)
+
+    # Post process
+    volperatom_arr = vol_arr/n_arr
+    density_arr = 1./volperatom_arr
+    d_average = np.mean(density_arr)
+    d_std = np.std(density_arr)
+    v_average = np.mean(volperatom_arr)
+    v_std = np.std(volperatom_arr)
+        
+    ## plot
+    from matplotlib import pyplot as plt
+    font = {'family':'Arial'}
+    plt.rc('font', **font)
+    # Density
+    n, bins, patches = plt.hist(density_arr, bins=100, facecolor='gray', alpha=0.70)
+    max_height = np.sort(n)[-10]
+    plt.title('Density Histogram (atoms/Ang^3)', fontsize='x-large')
+    plt.xlabel('%d images, average = %.4e, sigma = %.4e' % (len(density_arr), d_average, d_std), fontsize='x-large')
+    plt.ylabel('population', fontsize='x-large')
+    plt.barh(max_height/5, d_std, height=max_height/50, left=d_average, color='black')
+    plt.axvline(d_average, c='k')
+    plt.tick_params(axis="both",direction="in", labelsize='x-large')
+    plt.grid(alpha=0.4)
+    # Volume
+    plt.figure()
+    n, bins, patches = plt.hist(volperatom_arr, bins=100, facecolor='gray', alpha=0.70)
+    max_height = np.sort(n)[-10]
+    plt.title('Volume Histogram (Ang^3/atom)', fontsize='x-large')
+    plt.xlabel('%d images, average = %.4e, sigma = %.4e' % (len(volperatom_arr), v_average, v_std), fontsize='x-large')
+    plt.ylabel('population', fontsize='x-large')
+    plt.barh(max_height/5, v_std, height=max_height/50, left=v_average, color='black')
+    plt.axvline(v_average, c='k')
+    plt.tick_params(axis="both",direction="in", labelsize='x-large')
+    plt.grid(alpha=0.4)
+    plt.show()
