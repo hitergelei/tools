7a46ca54059dc3ffded52a45de78fb28efa7fd5c
Youngjae Choi <ssrokyz@gmail.com>
2022-07-20 09:43:27 +0900

    t

--- ase-gui-spec.py
+++ ase-gui-spec.py
@@ -4,10 +4,11 @@ import numpy as np
 def argparse():
     import argparse
     parser = argparse.ArgumentParser(description = """
-    DESCRIPTION
+    View alist with specific species.
     """)
     # Positional arguments
-    parser.add_argument('alist_file_list', type=str, nargs='+', help='ASE readable atoms list file name. When multiple input files are provided, DOS will averaged.')
+    parser.add_argument('alist_file', type=str, help='ASE readable atoms list file name.')
+    parser.add_argument('spec', type=str, nargs='+', help='Chemical symbols to view.')
     # Optional arguments
     parser.add_argument('-n', '--img_slice', type=str, default=':', help='Image range following python convention. default=":" (e.g.) -n :1000:10')
     return parser.parse_args()
@@ -22,10 +23,16 @@ if __name__ == '__main__':
     print(('Code runtime : '+time).center(120))
     print('')
     print('=================================================================================================='.center(120))
-    print('DESCRIPTION'.center(120))
+    print(' View alist with specific species.'.center(120))
     print('=================================================================================================='.center(120))
     print('')
     args = argparse()
 
     from ase.io import read
     alist = read(args.alist_file, args.img_slice)
+
+    from ss_util import screen_species
+    new_alist = screen_species(alist, args.spec)
+
+    from ase.visualize import view
+    view(new_alist)
